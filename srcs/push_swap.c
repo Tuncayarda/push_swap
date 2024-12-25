b39fbf064cc4c56d99a9ead608a0f11ae0c8cd5e
Tuncay Arda Aydın <tuaydin@k1m01s07.42istanbul.com.tr>
2024-12-25 19:01:06 +0300

    test case

--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tuaydin <tuaydin@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 14:34:06 by tuaydin           #+#    #+#             */
-/*   Updated: 2024/12/25 16:38:25 by tuaydin          ###   ########.fr       */
+/*   Updated: 2024/12/25 18:25:15 by tuaydin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 int main(int ac, char *args[])
 {
 	t_stack *a;
+	t_stack *b;
 	t_stack	*tmp;
 	
 	a = ps_parse(ac, args);
-	ft_printf("--A--\n");
+	b = NULL;
+
+	ft_printf("--------A--------\n");
 	tmp = a;
 	if (tmp)
 		for (int i = 0; i < stack_size(a); i++)
@@ -26,6 +29,23 @@ int main(int ac, char *args[])
 			ft_printf("%d %d\n",i, tmp->val);
 			tmp = tmp->next;
 		}
+	sa(&a, 1);
+	pb(&a, &b);
+	pb(&a, &b);
+	pb(&a, &b);
+	rr(&a, &b);
+	rrr(&a, &b);
+	sa(&a, 1);
+	pa(&a, &b);
+	pa(&a, &b);
+	pa(&a, &b);
 	
+	tmp = a;
+	if (tmp)
+		for (int i = 0; i < stack_size(a); i++)
+		{
+			ft_printf("%d %d\n",i, tmp->val);
+			tmp = tmp->next;
+		}
 	stack_free(&a);
 }
